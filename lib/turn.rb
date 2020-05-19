dcbd8af6ded821effb59b51f5b92ea408f7c53a8
candescent-torrent-9701 <williamsmatthew333@gmail.com>
2020-05-19 02:32:04 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -45,7 +45,7 @@ input = gets.strip
 index = input_to_index(input)
   if valid_move?(board, index)
   move(board, index, "")
-elsif !valid_move(board, index, "")
+elsif !valid_move?(board, index, "")
   puts "Invalid input"
   puts "Please enter 1-9:"
   input = gets.strip
